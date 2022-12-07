7ac10b74c4558b75dda2ed82b4f585fb352fd72c
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2022-12-08 00:32:57 +0900

    init

---
+++ day1/calorie-counting/src/main.rs
@@ -0,0 +1,24 @@
+use std::{io::{self, Read}};
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_to_string(&mut input).expect("Failed to read line");
+
+    let lines = input.split('\n');
+
+    let mut calories : i64 = 0;
+    let mut max :i64 =  0;
+    
+    for line in lines {
+        if line.is_empty() {
+            max = if max > calories {max} else {calories};
+            calories = 0;
+            continue;
+        }
+        calories += line.parse::<i64>().unwrap();
+    }
+    println!("{}", max);
+
+    
+}
